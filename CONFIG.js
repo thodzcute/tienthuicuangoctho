cf5eecc1112a3f39ea40f1a23f38e5df2d7a8372
thodzcute <ngoctho19052004@gmail.com>
2024-08-20 19:07:41 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào Tiên , Thọ có điều muốn nói. </br> Tiên có thấy thọ yêu tiên dữ dằng hokk!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé.",
+  buttonYes: "Tiên có",
+  buttonNo: "Tiên hokkk",
+  titleModar: "Cảm ơn nợn của Thọ",
+  descriptionModar:
+    "Cảm ơn Tiên đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ Tiên. </br> Thọ biết Thọ không được đẹp trai, cũng chẳng lắm tài nhưng Thọ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tớ yêu cậu nhắm nhó làm gì tớ củng nghỉ tới cậu hết , cậu là cô gái xinh đẹp cute phô mai que hihi .</br> Love you <3",
+};
